656f9ae2f5a3b521eef32fe296e9d67622f54d4a
Vladislav Torgovtsev <vlad4990@gmail.com>
2025-04-01 15:54:48 +0300

    update image 3

--- components/Form.vue
+++ components/Form.vue
@@ -111,6 +111,7 @@
     <div v-if="isError" class="mt-1 text-center text-[#AA1515]">{{isError}}</div>
     <div class="mx-5 mt-10 text-2xl whitespace-break-spaces">Как только мы определимся<br/>с площадкой я скину тебе ссылку на тг канал<br/>с информацией.
     </div>
-    <NuxtImg width="221" height="306" src="/assets/img/little-kristina.png" format="webp" loading="lazy" class="-mt-15 ml-auto"/>
+<!--    <NuxtImg width="221" height="306" src="/assets/img/little-kristina.png" format="webp" loading="lazy" class="-mt-15 ml-auto"/>-->
+    <img src="/assets/img/little-kristina.png" loading="lazy" class="w-[221px] h-[306px] -mt-15 ml-auto"/>
   </div>
 </template>
